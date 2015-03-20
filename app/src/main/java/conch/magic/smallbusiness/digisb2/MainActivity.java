b177513ddd03e921143c3036e789a5aab329cca4
tldr193 <micmoo@me.com>
2015-03-20 13:58:53 -0400

    push by mikey

--- app/src/main/java/conch/magic/smallbusiness/digisb2/MainActivity.java
+++ app/src/main/java/conch/magic/smallbusiness/digisb2/MainActivity.java
@@ -3,7 +3,10 @@ package conch.magic.smallbusiness.digisb2;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.view.MenuItem;
+import android.content.Intent;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +38,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean OnStockRoomItemSelected(View item){
+        startActivity(new Intent(MainActivity.this, ListActivity.class));
+        return true;
     }
 }
