f1ac99c59344ef90a368997c9f8e46829eb51a7d
leef3 <lee_frederick@hotmail.com>
2015-04-08 23:46:05 -0400

    I'm scared of commitment

--- app/src/main/java/conch/magic/smallbusiness/digisb2/ExpenseActivity.java
+++ app/src/main/java/conch/magic/smallbusiness/digisb2/ExpenseActivity.java
@@ -3,25 +3,22 @@ package conch.magic.smallbusiness.digisb2;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
 import android.view.MenuItem;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ExpenseActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.expense_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_expense, menu);
         return true;
     }
 
@@ -39,11 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-    public boolean OnStockRoomItemSelected(View item){
-        startActivity(new Intent(MainActivity.this, ListActivity.class));
-        return true;
-    }
-
 }
